f1baae8cc87b12299792c0ba33d51696d0fc9406
Zyad <zyadgohary@hotmail.com>
2020-06-29 18:59:23 -0700

    Coded the Country.js and the County.js files

--- Code/App.js
+++ Code/App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// This is going to be the screen that is automatically navigated to by React Native. It will navigate to the rest
+// of the screens
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import Country from './src/classes/Country';
+import County from './src/classes/County';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Creates the functional component
+const App = (props) => {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		fetchFunc();
+	}, []);
+
+	const fetchFunc = async () => {
+    const kingCounty = new County('King', "Washington");
+    await kingCounty.initializeBasicData();
+    const history = await kingCounty.getHistoricalData(3);
+    console.log(history)
+    setIsLoading(false);
+	};
+
+  if (isLoading === true) {
+    return <Text>Loading</Text>
+  } else {
+    return <Text>Done</Text>
+  }
+	// Renders the UI
+	return <View />;
+};
+
+// Exports the module
+export default App;
