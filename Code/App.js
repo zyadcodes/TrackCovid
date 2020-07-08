8f2f3984fd22f2e677b5e9f58fb9c33218726222
Zyad <zyadgohary@hotmail.com>
2020-07-07 20:17:46 -0700

    Fixed final pod issues

--- Code/App.js
+++ Code/App.js
@@ -1,18 +1,22 @@
 // This is going to be the screen that is automatically navigated to by React Native. It will navigate to the rest
 // of the screens
 import React, { useEffect } from 'react';
+import { YellowError } from 'react-native';
 import MainStackNavigator from './src/MainStackNavigator';
 import codePush from 'react-native-code-push';
-import SplashScreen from 'react-native-splash-screen'
-
+import SplashScreen from 'react-native-splash-screen';
 
 // Creates the functional component
 let App = (props) => {
-
 	// Hides the SplashScreen
 	useEffect(() => {
 		SplashScreen.hide();
-	}, [])
+	}, []);
+
+	// Overrides the CodePush download progress method in order to avoid warnings
+	const codePushDownloadDidProgress = (progress) => {
+		console.log(progress.receivedBytes + ' of ' + progress.totalBytes + ' received.');
+	};
 
 	// Renders the App
 	return <MainStackNavigator />;
